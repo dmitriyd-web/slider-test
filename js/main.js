d70fd986611ead89c49d780565464071d55b919d
dmitriyd-web <yadimon1989@gmail.com>
2023-07-04 11:07:29 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		nav: true,
+		dots: true,
+		items: 1,
+		//autoplay:true,
+		//autoplayTimeout: 2000,
+		navText: ['<div class="prev"><img src="images/btn-arrow.png"></div>','<div class="next"><img src="images/btn-arrow.png"></div>']
+	});
+})
